2893d638bf8633c08785f7c3b79b3be5f2722cc3
GianpeCARP <gianluca18907@gmail.com>
2025-07-17 13:49:43 -0300

    Martingala 100%

--- ruleta.sh
+++ ruleta.sh
@@ -41,59 +41,94 @@ function martingala(){
 #  sleep 2
   tput civis #Ocultar el cursor
 
-  while true; do 
-    let play_counter+=1
-    if [ "$money" -lt "$initial_bet" ]; then
-      echo -e "\n${redColour}[!] Saldo insuficiente: $initial_bet$ (apuesta) > $money$ (monto)${endColour}"
-      echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Se han hecho${endColour} ${blueColour}$play_counter${endColour}${greyColour} jugadas${endColour}"
-      echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Malas jugadas consecutivas:${endColour}"
-      echo -e "\n${blueColour}$jugadas_malas${endColour}"
-      if [ "$mayor_pos" -eq 0 ]; then
-        echo -e "\n[+] La mayor cantidad de dinero que se tuvo fué la del monto inicial: $mayor_cant$ en la jugada $mayor_pos"
-      else
-        echo -e "\n[+] La mayor cantidad de dinero que se tuvo fué de: $mayor_cant$ en la jugada $mayor_pos"
-      fi 
-      tput cnorm; exit 0
-    fi
-    
-    echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Bet:${endColour} ${greenColour}$initial_bet\$${endColour} ${purpleColour}/${endColour} ${greyColour}Dinero actual:${endColour} ${greenColour}$money\$${endColour}\n"
-    money=$(($money-$initial_bet))
-    random_number="$(($RANDOM % 37))"
-#    echo -e "${yellowColour}[+]${endColour} ${greyColour}Ha salido el número${endColour} ${blueColour}$random_number${endColour}"
-    
-    if [ "$par_impar" == "par" ]; then
-      if [ "$(($random_number % 2))" -eq 0 ]; then
-        if [ "$random_number" -eq 0 ]; then
-#          echo -e "\n${redColour}[!] Salió el 0, perdiste${endColour}"
-#          echo -e "${redColour}[!] Pérdida: $initial_bet\$${endColour}"
+  #Verificación de datos
+  if [[ "$initial_bet" =~ ^[1-9][0-9]*$ ]] && ! [[ "$par_impar" =~ ^[0-9]+$ ]]; then
+    #Arranca el bucle
+    while true; do 
+      let play_counter+=1
+      if [ "$money" -lt "$initial_bet" ]; then
+        echo -e "\n${redColour}[!] Saldo insuficiente: $initial_bet$ (apuesta) > $money$ (monto)${endColour}"
+        echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Se han hecho${endColour} ${blueColour}$(($play_counter-1))${endColour}${greyColour} jugadas${endColour}"
+        echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Malas jugadas consecutivas:${endColour}"
+        echo -e "\n${blueColour}$jugadas_malas]${endColour}"
+        if [ "$mayor_pos" -eq 0 ]; then
+          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}La mayor cantidad de dinero que se tuvo fué la del monto inicial:${endColour} ${greenColour}$mayor_cant\$${endColour} ${greyColour}en la jugada${endColour} ${blueColour}$mayor_pos${endColour}"
+        else
+          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}La mayor cantidad de dinero que se tuvo fué de:${endColour} ${greenColour}$mayor_cant\$${endColour} ${greyColour}en la jugada${endColour} ${blueColour}$mayor_pos${endColour}"
+        fi 
+        tput cnorm; exit 0
+      fi
+      
+  #    echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Bet:${endColour} ${greenColour}$initial_bet\$${endColour} ${purpleColour}/${endColour} ${greyColour}Dinero actual:${endColour} ${greenColour}$money\$${endColour}\n"
+      money=$(($money-$initial_bet))
+      random_number="$(($RANDOM % 37))"
+  #    echo -e "${yellowColour}[+]${endColour} ${greyColour}Ha salido el número${endColour} ${blueColour}$random_number${endColour}"
+      
+      if [ "$par_impar" == "par" ]; then
+        #PARES
+        if [ "$(($random_number % 2))" -eq 0 ]; then
+          if [ "$random_number" -eq 0 ]; then
+  #          echo -e "\n${redColour}[!] Salió el 0, perdiste${endColour}"
+  #          echo -e "${redColour}[!] Pérdida: $initial_bet\$${endColour}"
+            initial_bet=$(($initial_bet*2))
+  #          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes:${endColour} ${greenColour}$money\$${endColour}"
+  #          echo -e "\n----------------------------------------------------------------------------------------------------"
+            jugadas_malas+="$random_number "
+        else
+  #          echo -e "\n${turquoiseColour}[+] Salió un número par, ¡GANAS!${endColour}"
+            reward=$(($initial_bet*2))
+  #          echo -e "${turquoiseColour}[+] Beneficio:${endColour} ${greenColour}$reward\$${endColour}"
+            money=$(($money+$reward))
+  #          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
+            initial_bet=$backup_bet
+  #          echo -e "\n----------------------------------------------------------------------------------------------------"
+            jugadas_malas="[ "
+            if [ ! $money -le $mayor_cant ]; then
+              mayor_cant=$money
+              mayor_pos=$play_counter
+            fi
+          fi
+        else
+  #        echo -e "\n${redColour}[!] Salió un número impar, perdiste${endColour}"
+  #        echo -e "${redColour}[!] Pérdida: $initial_bet\$${endColour}"
           initial_bet=$(($initial_bet*2))
-#          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes:${endColour} ${greenColour}$money\$${endColour}"
-#          echo -e "\n----------------------------------------------------------------------------------------------------"
+  #        echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
+  #        echo -e "\n----------------------------------------------------------------------------------------------------"
           jugadas_malas+="$random_number "
-       else
-#          echo -e "\n${turquoiseColour}[+] Salió un número par, ¡GANAS!${endColour}"
+        fi
+
+      elif [ "$par_impar" == "impar" ]; then
+        #IMPAR
+        if [ "$(($random_number % 2))" -eq 1 ]; then
+  #          echo -e "\n${turquoiseColour}[+] Salió un número impar, ¡GANAS!${endColour}"
           reward=$(($initial_bet*2))
-#          echo -e "${turquoiseColour}[+] Beneficio:${endColour} ${greenColour}$reward\$${endColour}"
+  #          echo -e "${turquoiseColour}[+] Beneficio:${endColour} ${greenColour}$reward\$${endColour}"
           money=$(($money+$reward))
-#          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
+  #          echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
           initial_bet=$backup_bet
-#          echo -e "\n----------------------------------------------------------------------------------------------------"
-          jugadas_malas=""
+  #          echo -e "\n----------------------------------------------------------------------------------------------------"
+          jugadas_malas="[ "
           if [ ! $money -le $mayor_cant ]; then
             mayor_cant=$money
             mayor_pos=$play_counter
           fi
+        else
+  #        echo -e "\n${redColour}[!] Salió un número par, perdiste${endColour}"
+  #        echo -e "${redColour}[!] Pérdida: $initial_bet\$${endColour}"
+        initial_bet=$(($initial_bet*2))
+  #        echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
+  #        echo -e "\n----------------------------------------------------------------------------------------------------"
+        jugadas_malas+="$random_number "
         fi
       else
-#        echo -e "\n${redColour}[!] Salió un número impar, perdiste${endColour}"
-#        echo -e "${redColour}[!] Pérdida: $initial_bet\$${endColour}"
-        initial_bet=$(($initial_bet*2))
-#        echo -e "\n${yellowColour}[+]${endColour} ${greyColour}Tienes: ${endColour}${greenColour}$money\$${endColour}"
-#        echo -e "\n----------------------------------------------------------------------------------------------------"
-        jugadas_malas+="$random_number "
+        echo -e "\n${redColour}[!]${endColour} ${greyColour}Valores válidos${endColour} ${blueColour}->${endColour} ${purpleColour}(${endColour}${turquoiseColour}par${endColour}${purpleColour}/${endColour}${turquoiseColour}impar${endColour}${purpleColour})${endColour}"
+        exit 1
       fi
-    fi
-  done
+    done
+  else
+    echo -e "\n${redColour}[!] Asegurate de ingresar valores válidos${endColour}"
+    exit 1
+  fi
 
   tput cnorm #Recuperar el cursor
 }
